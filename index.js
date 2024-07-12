7636fd8b2587678692b58c460e76b45ea8ceb302
Arthur Lin <arthurlin2001@gmail.com>
2024-07-12 16:51:29 -0400

    Delete text-to-speech

--- index.js
+++ index.js
@@ -60,17 +60,6 @@ icons.forEach(icon => {
             } else {
                 navigator.clipboard.writeText(toText.value);
             }
-        } else {
-            // Text to speech events
-            let utterance;
-            if (target.id == "from") {
-                utterance = new SpeechSynthesisUtterance(fromText.value);
-                utterance.lang = selectTag[0].value;
-            } else {
-                utterance = new SpeechSynthesisUtterance(toText.value);
-                utterance.lang = selectTag[1].value; 
-            }
-            speechSynthesis.speal(utterance);
         }
     });
 });
